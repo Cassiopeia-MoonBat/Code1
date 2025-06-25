73c86735ab4bfffd211b480721c1dd24556adba6
Mina-Luna <boettchermina05@gmail.com>
2025-06-25 11:38:32 +0200

    ts

--- TheCode/code.js
+++ TheCode/code.js
@@ -1,2 +1,9 @@
 "use strict";
 console.log("The code never sleeps");
+let v = 1;
+v = v + 1;
+console.log(v);
+let a = [7, true, "hello"];
+console.log(a[1]);
+const b = 1;
+console.log(b);
--- TheCode/code.ts
+++ TheCode/code.ts
@@ -1,1 +1,11 @@
 console.log("The code never sleeps");
+
+let v: number = 1;
+v = v + 1;
+console.log(v);
+
+let a = [7, true, "hello"];
+console.log(a[1]);
+
+const b: number = 1;
+console.log(b);
