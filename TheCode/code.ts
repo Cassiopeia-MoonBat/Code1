4eff8ba5bf77f4d415df206e4f152867738f6811
Mina-Luna <boettchermina05@gmail.com>
2025-06-25 14:08:18 +0200

    simon verarscht mich

--- TheCode/code.ts
+++ TheCode/code.ts
@@ -9,3 +9,21 @@ console.log(a[1]);
 
 const b: number = 1;
 console.log(b);
+
+
+
+let x: number = 4;
+let y: number = 3;
+let temp: number;
+
+console.log(x);
+console.log(y);
+temp = y;
+y = x;
+x = temp;
+console.log(x);
+console.log(y);
+
+
+
+
