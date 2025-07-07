4c46a5297b14a480ba76d2f87df6ae2da66074d2
Mina-Luna <boettchermina05@gmail.com>
2025-07-07 17:33:31 +0200

    ballz

--- Projects/balling/ball.ts
+++ Projects/balling/ball.ts
@@ -7,29 +7,34 @@ namespace Ballz {
         velocity: Vector
     }
     window.addEventListener("load", iwannago);
-    let ball: Ball;
+    let balls: Ball[] = [];
 
 
 
     function iwannago(): void {
-        ball = {
-        element: document.createElement("span"),
-        position:  { x: 100, y: 100 },
-        velocity:  { x: 2, y: 2 }
-        };
-        document.body.appendChild(ball.element);
+        for (let i = 0; i < Math.floor(Math.random() * 20) + 10; i++) {
+            let ball : Ball = {
+                element: document.createElement("span"),
+                position: { x: Math.floor(Math.random() * 2000) + 1, y: Math.floor(Math.random() * 2000) + 1 },
+                velocity: { x: Math.floor(Math.random() * 20) + 1, y: Math.floor(Math.random() * 10) + 1 }
+            };
+            balls.push(ball);
+            document.body.appendChild(ball.element);
+        }
         move();
     }
 
 
     function move() {
-        ball.position.x += ball.velocity.x;
-        ball.position.y += ball.velocity.y;
+        for (let ball of balls) {
+            ball.position.x += ball.velocity.x;
+            ball.position.y += ball.velocity.y;
 
-        ball.position.x = (ball.position.x + window.innerWidth) % window.innerWidth;
-        ball.position.y = (ball.position.y + window.innerHeight) % window.innerHeight;
+            ball.position.x = (ball.position.x + window.innerWidth) % window.innerWidth;
+            ball.position.y = (ball.position.y + window.innerHeight) % window.innerHeight;
 
-        ball.element.style.transform = "matrix(10, 0, 0, 10, " + ball.position.x + "," + ball.position.y + ")";
+            ball.element.style.transform = "matrix(10, 0, 0, 10, " + ball.position.x + "," + ball.position.y + ")";
+        }
         setTimeout(move, 16);
     }
 
