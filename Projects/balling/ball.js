65e7ac3cabf728cf9ed476fa424522232e175255
Mina-Luna <boettchermina05@gmail.com>
2025-07-08 12:33:13 +0200

    update of ballz. create, delete

--- Projects/balling/ball.js
+++ Projects/balling/ball.js
@@ -5,18 +5,43 @@ var Ballz;
     let balls = [];
     let timePreviousFrame = Date.now();
     function iwannago() {
-        for (let i = 0; i < Math.floor(Math.random() * 20) + 10; i++) {
+        document.body.addEventListener("click", hndClick);
+        for (let i = 0; i < Math.floor(Math.random() * 3) + 1; i++) {
             let ball = {
                 element: document.createElement("span"),
                 position: { x: Math.floor(Math.random() * 2000) + 1, y: Math.floor(Math.random() * 2000) + 1 },
-                velocity: { x: Math.floor(Math.random() * 500) - 250, y: Math.floor(Math.random() * 500) - 250 }
+                velocity: { x: Math.floor(Math.random() * 50) - 25, y: Math.floor(Math.random() * 50) - 25 }
             };
             balls.push(ball);
             document.body.appendChild(ball.element);
         }
-        move();
+        move(0);
     }
-    function move() {
+    function hndClick(_event) {
+        const element = _event.target;
+        let name = element.tagName;
+        console.log(name);
+        console.log(element);
+        if (name == "SPAN") {
+            document.body.removeChild(element);
+            for (let ball of balls) {
+                if (ball.element == element) {
+                    balls.splice(balls.indexOf(ball, 1));
+                    console.log(ball);
+                }
+            }
+        }
+        else {
+            let ball = {
+                element: document.createElement("span"),
+                position: { x: _event.pageX, y: _event.pageY },
+                velocity: { x: Math.floor(Math.random() * 200) - 100, y: Math.floor(Math.random() * 200) - 100 }
+            };
+            balls.push(ball);
+            document.body.appendChild(ball.element);
+        }
+    }
+    function move(_deltaTime) {
         const timeCurrent = Date.now();
         const timeDelta = timeCurrent - timePreviousFrame;
         for (let ball of balls) {
@@ -27,7 +52,8 @@ var Ballz;
             ball.element.style.transform = "matrix(10, 0, 0, 10, " + ball.position.x + "," + ball.position.y + ")";
         }
         timePreviousFrame = timeCurrent;
-        setTimeout(move, 16);
+        //setTimeout(move, 16);
+        requestAnimationFrame(move);
     }
 })(Ballz || (Ballz = {}));
 //# sourceMappingURL=ball.js.map
--- Projects/balling/ball.ts
+++ Projects/balling/ball.ts
@@ -12,33 +12,60 @@ namespace Ballz {
 
 
     function iwannago(): void {
-        for (let i = 0; i < Math.floor(Math.random() * 20) + 10; i++) {
-            let ball : Ball = {
+        document.body.addEventListener("click", hndClick);
+
+        for (let i = 0; i < Math.floor(Math.random() * 3) + 1; i++) {
+            let ball: Ball = {
                 element: document.createElement("span"),
                 position: { x: Math.floor(Math.random() * 2000) + 1, y: Math.floor(Math.random() * 2000) + 1 },
-                velocity: { x: Math.floor(Math.random() * 500) - 250, y: Math.floor(Math.random() * 500) - 250 }
+                velocity: { x: Math.floor(Math.random() * 50) - 25, y: Math.floor(Math.random() * 50) - 25 }
             };
             balls.push(ball);
             document.body.appendChild(ball.element);
         }
-        move();
+        move(0);
     }
 
+    function hndClick(_event: MouseEvent): void {
+        const element = _event.target as HTMLElement;
+        let name = element.tagName;
+        console.log(name);
+        console.log(element);
+        if (name == "SPAN") {
+            document.body.removeChild(element)
+            for (let ball of balls) {
+                if (ball.element == element) {
+                    balls.splice(balls.indexOf(ball,1));
+                    console.log(ball)
+                }
+            }
+        }
+        else {
+            let ball: Ball = {
+                element: document.createElement("span"),
+                position: { x: _event.pageX, y: _event.pageY },
+                velocity: { x: Math.floor(Math.random() * 200) - 100, y: Math.floor(Math.random() * 200) - 100 }
+            };
+            balls.push(ball);
+            document.body.appendChild(ball.element);
+        }
+    }
 
-    function move() {
+    function move(_deltaTime: number) {
         const timeCurrent: number = Date.now();
         const timeDelta: number = timeCurrent - timePreviousFrame;
         for (let ball of balls) {
-            ball.position.x += (ball.velocity.x * (timeDelta /1000));
-            ball.position.y += (ball.velocity.y * (timeDelta /1000));
+            ball.position.x += (ball.velocity.x * (timeDelta / 1000));
+            ball.position.y += (ball.velocity.y * (timeDelta / 1000));
 
             ball.position.x = (ball.position.x + window.innerWidth) % window.innerWidth;
             ball.position.y = (ball.position.y + window.innerHeight) % window.innerHeight;
 
             ball.element.style.transform = "matrix(10, 0, 0, 10, " + ball.position.x + "," + ball.position.y + ")";
         }
-        timePreviousFrame = timeCurrent ;
-        setTimeout(move, 16);
+        timePreviousFrame = timeCurrent;
+        //setTimeout(move, 16);
+        requestAnimationFrame(move);
     }
 
 }
